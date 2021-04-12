dd309627c40c308e54da268805de26a4cd44fefb
Malin Jobb <malinhansson@Malins-MacBook-Pro-416.local>
2021-04-12 13:20:06 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Performs House Price Predictions',
+    author='Oskar Enhörning',
+    license='MIT',
+)
